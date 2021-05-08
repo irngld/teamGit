19fce6eb853719ba22f8d084a3d26f0d07e64f19
jackiejones3bb <jackiejones3bb@gmail.com>
2021-05-08 10:36:58 -0400

    fixed temp conversion and .env

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import './App.css';
-import React from 'react';
+import "./App.css";
+import React from "react";
 
-import 'bootstrap/dist/css/bootstrap.css';
-import bigBrother from './utils/geoLocation';
-
+import "bootstrap/dist/css/bootstrap.css";
+import bigBrother from "./utils/geoLocation";
 
 // import Header from './components/Header'
 // import Footer from './components/Footer'
 // import SatelliteApp from './components/SatelliteImg';
-import WeatherImg from './components/WeatherImg';
+import WeatherImg from "./components/WeatherImg";
 
 function App() {
-
   const coords = bigBrother();
   console.log(coords);
 
   return (
-    <div className="App">
-     {/* <Header /> */}
-        <WeatherImg coords={coords}/>
-     {/* <Footer /> */}
+    <div className='App'>
+      {/* <Header /> */}
+      <WeatherImg coords={coords} />
+      {/* <Footer /> */}
     </div>
   );
 }
--- src/utils/tempConv.js
+++ src/utils/tempConv.js
@@ -1,11 +1,10 @@
-
 const tempConv = (temp) => {
-    let fTemp = (temp - 273) * 1.8 + 32;
-    let fTempFixed = fTemp.toFixed(0);
-    let cTemp = temp - 273;
-    let cTempFixed = cTemp.toFixed(0);
+  let fTemp = (temp - 273) * 1.8 + 32;
+  let fTempFixed = fTemp.toFixed(0);
+  let cTemp = temp - 273;
+  let cTempFixed = cTemp.toFixed(0);
 
-    return { celsius: 12, farenheit: 53 }
-}
+  return { celsius: cTempFixed, farenheit: fTempFixed };
+};
 
 export default tempConv;
